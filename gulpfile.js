272597a12fc6848b878c47b718604d96793a854f
Sergei Baranov <dev@rkkmkkfx.ru>
2021-12-08 00:11:26 +0800

    8.11.0 released, update component's version in the localization-demo solution, fix json tab

--- gulpfile.js
+++ gulpfile.js
@@ -11,73 +11,73 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 var config = {
-    sourceDir: './src/',
-    publicDir: './dist/'
+  sourceDir: './src/',
+  publicDir: './dist/'
 };
 
 var filesToMove = [
-    './src/js/plugins/**/*.*'
+  './src/js/plugins/**/*.*'
 ];
 
 /* Scripts task */
 gulp.task('scripts', function () {
-    var jsDir = config.publicDir + 'js';
+  var jsDir = config.publicDir + 'js';
 
-    return gulp.src(config.sourceDir + 'js/*.js')
-        .pipe(concat('common.js'))
-        .pipe(gulp.dest(jsDir))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDir));
+  return gulp.src(config.sourceDir + 'js/*.js')
+    .pipe(concat('common.js'))
+    .pipe(gulp.dest(jsDir))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDir));
 });
 
 /* Styles task */
 gulp.task('styles', function () {
-    var cssDir = config.publicDir + 'css';
+  var cssDir = config.publicDir + 'css';
 
-    return gulp.src(config.sourceDir + '/sass/app.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('.'))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest(cssDir))
-        .pipe(cleanCSS())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(cssDir))
+  return gulp.src(config.sourceDir + '/sass/app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(cssDir))
+    .pipe(cleanCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(cssDir))
 });
 
 /* Copy files */
-gulp.task('move', function(){
-    // the base option sets the relative root for the set of files,
-    // preserving the folder structure
-    gulp.src(filesToMove, { base: './src/js/plugins' })
-        .pipe(gulp.dest('./dist/js/plugins/'));
+gulp.task('move', function () {
+  // the base option sets the relative root for the set of files,
+  // preserving the folder structure
+  gulp.src(filesToMove, {base: './src/js/plugins'})
+    .pipe(gulp.dest('./dist/js/plugins/'));
 });
 
 // Static Server + watching scss/html files
 gulp.task('serve', function () {
 
-    browserSync.init({
-        server: "./"
-    });
+  browserSync.init({
+    server: "./"
+  });
 
-    gulp.watch(config.sourceDir + 'sass/**/*.*', ['styles']);
-    gulp.watch(config.sourceDir + 'js/**/*.*', ['scripts']);
-    gulp.watch(config.publicDir + 'css/*.css').on('change', browserSync.reload);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch(config.sourceDir + 'sass/**/*.*', ['styles']);
+  gulp.watch(config.sourceDir + 'js/**/*.*', ['scripts']);
+  gulp.watch(config.publicDir + 'css/*.css').on('change', browserSync.reload);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 
 /* Reload task */
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 /* Watch scss, js and html files, doing different things with each. */
 gulp.task('default', ['scripts', 'styles', 'move', 'serve'], function () {
-    /* Watch scss, run the sass task on change. */
-    gulp.watch(config.sourceDir + 'sass/**/*.*', ['styles']);
-    /* Watch app.js file, run the scripts task on change. */
-    gulp.watch(config.sourceDir + 'js/**/*.*', ['scripts']);
-    /* Watch .html files, run the bs-reload task on change. */
-    gulp.watch(['*.html'], ['bs-reload']);
+  /* Watch scss, run the sass task on change. */
+  gulp.watch(config.sourceDir + 'sass/**/*.*', ['styles']);
+  /* Watch app.js file, run the scripts task on change. */
+  gulp.watch(config.sourceDir + 'js/**/*.*', ['scripts']);
+  /* Watch .html files, run the bs-reload task on change. */
+  gulp.watch(['*.html'], ['bs-reload']);
 });
